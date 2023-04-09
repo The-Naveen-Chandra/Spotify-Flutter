a52d0b0f6658ea5f953f298873732a0a4e61beeb
Naveen Chandra <the.naveenchandra@gmail.com>
2023-04-10 00:49:49 +0530

    ScrollBar added in the side menu bar.

--- lib/widgets/side_menu.dart
+++ lib/widgets/side_menu.dart
@@ -42,7 +42,7 @@ class SideMenu extends StatelessWidget {
           const SizedBox(
             height: 12.0,
           ),
-          // _LibraryPlaylist(),
+          _LibraryPlaylist(),
         ],
       ),
     );
@@ -80,43 +80,96 @@ class _SideMenuIconTab extends StatelessWidget {
 }
 
 class _LibraryPlaylist extends StatefulWidget {
+  const _LibraryPlaylist({super.key});
+
   @override
-  __LibraryPlaylistState createState() => __LibraryPlaylistState();
+  State<_LibraryPlaylist> createState() => __LibraryPlaylistState();
 }
 
 class __LibraryPlaylistState extends State<_LibraryPlaylist> {
+
+  ScrollController? _scrollController;
+
+  @override
+  void initstate() {
+    super.initState();
+    _scrollController = ScrollController();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _scrollController?.dispose();
+    super.dispose();
+  }
+ 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      padding: const EdgeInsets.symmetric(
-        vertical: 12.0,
-      ),
-      physics: const ClampingScrollPhysics(),
-      children: [
-        Column(
+    return Expanded(
+      child: Scrollbar(
+        thumbVisibility: true,
+        controller: _scrollController,
+
+        child: ListView(
+          controller: _scrollController,
+          padding: const EdgeInsets.symmetric(vertical: 12.0,),
+          physics: const ClampingScrollPhysics(),
           children: [
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-              child: Text(
-                "Your Library",
-                style: Theme.of(context).textTheme.bodySmall,
-                overflow: TextOverflow.ellipsis,
-              ),
-            ),
-            ...yourLibrary.map((e) => 
-              ListTile(
-                dense: true,
-                title: 
-                  Text(
-                  e,
-                  style: Theme.of(context).textTheme.bodyMedium,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                  child: Text(
+                    "Your Library",
+                    style: Theme.of(context).textTheme.bodySmall,
+                    overflow: TextOverflow.ellipsis,
+                  ),
                 ),
-              ),
-            ).toList(),
+                ...yourLibrary.map((e) => 
+                  ListTile(
+                    dense: true,
+                    title: 
+                      Text(
+                      e,
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
+                  ),
+                ).toList(),
+              ],
+            ),
+      
+            const SizedBox(height: 24.0,),
+      
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                  child: Text(
+                    "Playlist",
+                    style: Theme.of(context).textTheme.bodySmall,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                ...playlists.map((e) => 
+                  ListTile(
+                    dense: true,
+                    title: 
+                      Text(
+                      e,
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
+                  ),
+                ).toList(),
+              ],
+            ),
+      
           ],
         ),
-      ],
+      ),
     );
   }
 }
